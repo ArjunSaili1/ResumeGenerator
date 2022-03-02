af6a000b78c3e2bc45c94c9f36a53da874e4e95d
Arjun Saili <arjun7.saili@gmail.com>
2022-03-02 02:28:35 -0500

    Begin Store Resume Info as State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from "react";
+import ResumeForm from "./components/ResumeForm";
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      generalInfo:{
+        name: "",
+        num: "",
+        email: "",
+        address: "",
+      },
+      educationInfo:[],
+      experienceInfo:[]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <ResumeForm></ResumeForm>
+      </div>
+    ); 
+  }
 }
 
 export default App;
