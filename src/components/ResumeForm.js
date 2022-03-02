df31a9ddf6e33339551c99feedada6cb8fb00dad
Arjun Saili <arjun7.saili@gmail.com>
2022-03-02 02:28:57 -0500

    Create Resume Form Component

---
+++ src/components/ResumeForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class ResumeForm extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div className="resume-form">
+                hello
+            </div>
+        )
+    }
+}
+
+export default ResumeForm
