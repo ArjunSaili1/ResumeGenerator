c7e8a10f141e79f54a5e6e060a0e1a77f6c245ed
Arjun Saili <arjun7.saili@gmail.com>
2022-03-03 03:30:23 -0500

    Create Unique Edu State Object to Store Info

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,20 +1,47 @@
 import React, { Component } from "react";
+import uniqid from "uniqid";
 
 class EducationSection extends Component {
+
     constructor(props){
         super(props)
+        this.stateKey = uniqid();
+        this.state = {
+            [this.stateKey]: {
+                programName: "",
+                schoolName: "",
+                eduFromYear: "",
+                eduToYear: "",
+                gpa: "",
+            }
+        }
+        this.updateEduInfo = this.updateEduInfo.bind(this);
+    }
+
+
+    updateEduInfo(e){
+        const stateKey = this.stateKey
+        if(e.target.name in this.state[stateKey]){
+            this.setState({
+                [stateKey]: {
+                    ...this.state[stateKey],
+                    [e.target.name]: e.target.value
+                }
+            })
+        }
     }
 
     render(){
+        console.log(this.props)
         return(
             <div>
                 <h3>Education</h3>
                 <div>
-                    <input type="text" name="programName" placeholder="Program/Field of Study"></input>
-                    <input type="text" name="schoolName" placeholder="Institution Name"></input>
-                    <input type="number" name="eduFromYear" placeholder="From"></input>
-                    <input type="number" name="eduToYear" placeholder="To"></input>
-                    <input type="number" name="gpa" placeholder="GPA"></input>
+                    <input type="text" name="programName" placeholder="Program/Field of Study" onChange={this.updateEduInfo}></input>
+                    <input type="text" name="schoolName" placeholder="Institution Name" onChange={this.updateEduInfo}></input>
+                    <input type="number" name="eduFromYear" placeholder="From" onChange={this.updateEduInfo}></input>
+                    <input type="number" name="eduToYear" placeholder="To" onChange={this.updateEduInfo}></input>
+                    <input type="number" name="gpa" placeholder="GPA" onChange={this.updateEduInfo}></input>
                 </div>
             </div>
         )
