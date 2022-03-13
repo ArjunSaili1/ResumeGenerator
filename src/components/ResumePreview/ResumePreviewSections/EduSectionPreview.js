eba16050c54843f6af745c36a0a5ea2960ece8a6
Arjun Saili <arjun7.saili@gmail.com>
2022-03-13 13:08:44 -0400

    Update styling for preview sections

--- src/components/ResumePreview/ResumePreviewSections/EduSectionPreview.js
+++ src/components/ResumePreview/ResumePreviewSections/EduSectionPreview.js
@@ -4,16 +4,19 @@ class EduSectionPreview extends Component{
 
     render(){
         return(
-            <div>
-                <h2>Education</h2>
+            <div className="preview-section">
+                <h2 className="section-title">Education</h2>
+                <hr></hr>
                 {
                     Object.keys(this.props.info).map((section)=>{
                         return(
-                            <div key={section} className="preview-section">
-                                <h5>{this.props.info[section].programName}</h5>
-                                <h5>{this.props.info[section].schoolName}</h5>
-                                <h5>{this.props.info[section].gpa}</h5>
-                                <h5>{this.props.info[section].fromYear} - {this.props.info[section].toYear}</h5>
+                            <div className="preview-section-fields" key={section}>
+                                <h5 className="date-fields">{this.props.info[section].fromYear} - {this.props.info[section].toYear}</h5>
+                                <div className="info-fields">
+                                    <h3>{this.props.info[section].programName}</h3>
+                                    <h5>{this.props.info[section].schoolName}</h5>
+                                    <h6>{this.props.info[section].gpa} GPA</h6>
+                                </div>
                             </div>
                         )
                     })
--- src/components/ResumePreview/ResumePreviewSections/ExpSectionPreview.js
+++ src/components/ResumePreview/ResumePreviewSections/ExpSectionPreview.js
@@ -4,16 +4,20 @@ class ExpPreviewSection extends Component{
 
     render(){
         return(
-            <div>
-                <h2>Experience</h2>
+            <div className="preview-section">
+                <h2 className="section-title">Experience</h2>
+                <hr></hr>
                 {
                     Object.keys(this.props.info).map((section)=>{
+                        console.log(this.props.info[section])
                         return(
-                            <div key={section} className="preview-section">
-                                <h5>{this.props.info[section].position}</h5>
-                                <h5>{this.props.info[section].company}</h5>
-                                <h5>{this.props.info[section].expLocation}</h5>
-                                <h5>{this.props.info[section].fromYear} - {this.props.info[section].toYear}</h5>
+                            <div className="preview-section-fields" key={section}>
+                                <h5 className="date-fields">{this.props.info[section].fromYear} - {this.props.info[section].toYear}</h5>
+                                <div className="info-fields">
+                                    <h3>{this.props.info[section].position}</h3>
+                                    <h5>{this.props.info[section].company}</h5>
+                                    <h6>{this.props.info[section].expLocation}</h6>
+                                </div>
                             </div>
                         )
                     })
