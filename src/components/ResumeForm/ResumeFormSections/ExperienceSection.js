2cc4b100fedc1f6c277c4da2d459d98c67c07766
Arjun Saili <arjun7.saili@gmail.com>
2022-03-13 13:08:34 -0400

    Fix incorrect experience from/to field names bug

--- src/components/ResumeForm/ResumeFormSections/ExperienceSection.js
+++ src/components/ResumeForm/ResumeFormSections/ExperienceSection.js
@@ -9,10 +9,10 @@ class ExperienceSection extends Component {
                     <input className="section-input" type="text" name="position" placeholder="Position" onChange={this.props.updateInfo}></input>
                     <input className="section-input" type="text" name="company" placeholder="Company" onChange={this.props.updateInfo}></input>
                     <input className="section-input" type="text" name="expLocation" placeholder="Location" onChange={this.props.updateInfo}></input>
-                    <input className="section-input" type="number" name="expFromYear" placeholder="From" onChange={this.props.updateInfo}></input>
+                    <input className="section-input" type="number" name="fromYear" placeholder="From" onChange={this.props.updateInfo}></input>
                     {this.props.toField}
                     <div className="present section-input">
-                        <input id="present" type="checkbox" name="expToYear" defaultChecked onChange={this.props.setToField}></input>
+                        <input id="present" type="checkbox" name="toYear" defaultChecked onChange={this.props.setToField}></input>
                         <label htmlFor="present">Present </label>
                     </div>
                 </div>
